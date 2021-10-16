ff33be94cdb260bee9906302f70c665437ec330a
lokopak <pakpil@gmail.com>
2021-10-16 05:25:14 +0200

    Optimizados los estados de los socios

--- socios/model/socio.php
+++ socios/model/socio.php
@@ -1,9 +1,16 @@
 <?php
+
+const SOCIO_ESTADO_BAJA = 0;
+const SOCIO_ESTADO_ACTIVO = 1;
+const SOCIO_ESTADO_CON_LIBRO = 2;
+const SOCIO_ESTADO_SANCIONADO = 3;
+const SOCIO_ESTADO_SUSPENDIDO = 4;
+
 // Constante con los posibles estados de los socios
 const ESTADOS_SOCIO = [
-    0 => "Baja",
-    1 => "Activo",
-    2 => "Con libro",
-    3 => "Sancionado",
-    4 => "Suspendido"
+    SOCIO_ESTADO_BAJA => "Baja",
+    SOCIO_ESTADO_ACTIVO => "Activo",
+    SOCIO_ESTADO_CON_LIBRO => "Con libro",
+    SOCIO_ESTADO_SANCIONADO => "Sancionado",
+    SOCIO_ESTADO_SUSPENDIDO => "Suspendido"
 ];
