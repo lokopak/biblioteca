e3bc7cfd357f9a1ae461aaaa9872d351bfb2c614
pakpil@gmail.com <lokopak>
2021-10-16 04:16:54 +0200

    Socios / mostrar
    Se ha agregado la página para mostrar un socio.

--- socios/mostrar.php
+++ socios/mostrar.php
@@ -3,6 +3,35 @@
 // Mostramos el contenido html del head
 require_once(__DIR__ . "/../view/head.php");
 
+// Cargamos los datos necesarios para gestionar socios
+require_once(__DIR__ . "/model/socio.php");
+
+// Cargamos las funciones para recoger datos desde la Request.
+require_once(__DIR__ . "/../conexion/request.php");
+
+$idSocio = obtenerDesdeGet("idSocio", null);
+
+// Si no se proporciona ninguna idSocio, mostramos un error
+if (null === $idSocio) {
+    agregarError("La petición no es correcta.", "Error");
+    // Mostrar no encontrado.    
+    require_once(dirname(__FILE__) . "/../view/footer.php");
+    return;
+}
+
+// Cargamos el archivo necesario para manejar la tabla de socios.
+require_once(__DIR__ . "/model/db.php");
+
+// Buscamos el socio para comprobar que existe.
+$socio = sociosBuscarUno((int)$idSocio);
+
+// Si no existe el socio... mostramos la página de no encontrado
+if (null === $socio) {
+    require(__DIR__ . "/../view/no-encontrado.php");
+    return;
+}
+
+
 // Mostramos el contenido html de la página
 require(__DIR__ . "/view/mostrar.php");
 
---
+++ socios/view/mostrar.php
@@ -0,0 +1,33 @@
+<div class="row">
+    <div class="col-12 justify-content-center">
+        <div class="w-50 m-auto">
+
+            <h3>DNI</h3>
+            <input type="text" name="DNI" value="<?= $socio["DNI"] ?>">
+            <h3>Nombre</h3>
+            <input type="text" name="nombre" value="<?= $socio["nombre"] ?>">
+            <h3>Apellidos</h3>
+            <input type="text" name="apellidos" value="<?= $socio["apellidos"] ?>">
+            <h3>Fecha de nacimiento</h3>
+            <input type="date" name="fechaNacimiento" value="<?= $socio["fechaNacimiento"] ?>">
+            <h3>Dirección</h3>
+            <input type="text" name="direccion" value="<?= $socio["direccion"] ?>">
+            <h3>Teléfono</h3>
+            <input type="number" name="telefono" value="<?= $socio["telefono"] ?>">
+            <h3>E-mail</h3>
+            <input type="text" name="email" value="<?= $socio["email"] ?>">
+
+            <div class="mb-3">
+                <h3>Estado</h3>
+                <select class="form-select" aria-label="Estado" name="estado" value="<?= $socio["estado"] ?>">
+                    <option selected>Seleciona un estado</option>
+                    <option value="0"><?= $estadosSocio[0] ?></option>
+                    <option value="1"><?php echo $estadosSocio[1] ?></option>
+                    <option value="2"><?= $estadosSocio[2] ?></option>
+                    <option value="3"><?= $estadosSocio[3] ?></option>
+                    <option value="4"><?= $estadosSocio[4] ?></option>
+                </select>
+            </div>
+        </div>
+    </div>
+</div>
