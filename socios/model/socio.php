155f21b8d09005c8b04291112c1c9b115942bd32
pakpil@gmail.com <lokopak>
2021-10-16 04:21:22 +0200

    Agregado un modelo de socio
    El archivo model/socio contendrá todas las características exclusivas de un socio

---
+++ socios/model/socio.php
@@ -0,0 +1,9 @@
+<?php
+// Constante con los posibles estados de los socios
+const ESTADOS_SOCIO = [
+    0 => "Baja",
+    1 => "Activo",
+    2 => "Con libro",
+    3 => "Sancionado",
+    4 => "Suspendido"
+];
