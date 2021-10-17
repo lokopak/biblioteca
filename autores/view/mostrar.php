df413561cfc88cce696c32e21320a513e698f564
lokopak <pakpil@gmail.com>
2021-10-18 00:25:54 +0200

    Optimizada la plantilla de mostrar autor

--- autores/view/mostrar.php
+++ autores/view/mostrar.php
@@ -1,17 +1,22 @@
+<?php
+$nombreAutor = $autor["nombre"];
+if (isset($autor["apellidos"])) {
+    $nombreAutor .= " " . $autor["apellidos"];
+}
+?>
 <div class="row">
     <div class="col-12 col-sm-10 col-xl-6 mx-md-auto my-4">
         <div class="card rounded-top border-dark bg-transparent">
             <div class="card-title bg-naranja p-3 rounded-top mb-0">
                 <h1 class="mb-0"><i class="bi bi-vector-pen me-2 fs-1"></i>
-                    <?= $autor['nombre'] ?>
-                    <?= $autor['apellidos'] ?>
+                    <?= $nombreAutor ?>
                 </h1>
             </div>
             <div class="card-body bg-white rounded-bottom">
                 <div class="row mb-3">
                     <div class="col-12 col-md-4 mb-3">
                         <img src="/biblioteca/assets/images/person-no-image.png" class="img-fluid rounded-start"
-                            alt="<?= $autor['nombre'] ?> <?= $autor['apellidos'] ?>">
+                            alt="<?= $nombreAutor ?>">
                     </div>
                     <div class="col-12 col-md-8">
                         <p class="card-text">
